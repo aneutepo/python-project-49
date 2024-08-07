37b0e0677e64b51ff111bbebff342654b61c457a
Anton <poverenosn@gmail.com>
2024-08-07 22:49:29 +0300

    spet 6 completed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/aneutepo/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/aneutepo/python-project-49/actions)
+
+https://asciinema.org/a/iQqNscIMhGrqBCQnfGiYUrU18
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,40 @@
+import random
+from brain_games.cli import welcome_user
+
+
+def get_answer(first_number, second_number, math_sign):
+    if math_sign == "+":
+        return first_number + second_number
+    elif math_sign == "-":
+        return first_number - second_number
+    else:
+        return first_number * second_number
+
+
+def calc():
+    massive_of_signs = ['+', '-', '*']
+    score = 0
+    username = welcome_user()
+    while True:
+        first_number = random.randint(1, 6)
+        second_number = random.randint(1, 6)
+        math_sign = random.choice(massive_of_signs)
+        math_question = str(first_number) + math_sign + str(second_number)
+        print(f"Question: {math_question}")
+        user_answer = input("Your answer: ")
+        correct_answer = get_answer(first_number, second_number, math_sign)
+
+        if user_answer == str(correct_answer):
+            score += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. ", end='')
+            print(f"Correct answer was '{correct_answer}'")
+            break
+
+        if score == 3:
+            print(f"Congratulations, {username}")
+            break
+
+
+if __name__ == '__main__':
+    calc()
