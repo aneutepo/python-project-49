6f71c90f230f5df18bb5c6e6a7a903f86658e647
Anton <poverenosn@gmail.com>
2024-08-11 21:47:23 +0300

    added new functions to optimize main func

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,33 +2,37 @@ import random
 from brain_games.cli import welcome_user
 
 
+def get_correct_answer(question):
+    if question % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def check_user_answer(correct_answer, user_answer, username):
+    if user_answer == correct_answer:
+        print("Correct!")
+        return True
+    else:
+        print(f"Answer '{user_answer}' is wrong answer ;(. ", end='')
+        print(f"Correct answer was '{correct_answer}'.")
+        print(f"Let's try again, {username}!")
+        return False
+
+
 def main():
-    number_correct_answers = 0
+    score = 0
     username = welcome_user()
     print("Answer \"yes\" if the number is even, otherwise answer \"no\".")
     while True:
         question_number = random.randint(1, 10000)
         print(f"Question: {question_number}")
-        answer = input("Your answer: ")
-        if question_number % 2 == 0:
-            if answer == 'yes':
-                print("Correct!")
-                number_correct_answers += 1
-            else:
-                print(f"Answer '{answer}' is wrong answer ;(. ", end='')
-                print("Correct answer was 'yes'.")
-                print(f"Let's try again, {username}!")
-                break
-        else:
-            if answer == 'no':
-                print("Correct!")
-                number_correct_answers += 1
-            else:
-                print(f"Answer '{answer}' is wrong answer ;(. ", end='')
-                print("Correct answer was 'no'.")
-                print(f"Let's try again, {username}!")
-                break
-        if number_correct_answers == 3:
+        user_answer = input("Your answer: ")
+        correct_answer = get_correct_answer(question_number)
+        if check_user_answer(correct_answer, user_answer, username):
+            score += 1
+
+        if score == 3:
             print(f"Congratulations, {username}!")
             break
 
