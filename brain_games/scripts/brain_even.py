af6aff6b8ddd57de4b6759f9b4d02a45adf6d90b
Anton <poverenosn@gmail.com>
2024-08-11 21:59:30 +0300

    new logic

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -24,17 +24,14 @@ def main():
     score = 0
     username = welcome_user()
     print("Answer \"yes\" if the number is even, otherwise answer \"no\".")
-    while True:
+    while score < 3:
         question_number = random.randint(1, 10000)
         print(f"Question: {question_number}")
         user_answer = input("Your answer: ")
         correct_answer = get_correct_answer(question_number)
         if check_user_answer(correct_answer, user_answer, username):
             score += 1
-
-        if score == 3:
-            print(f"Congratulations, {username}!")
-            break
+    print(f"Congratulations, {username}!")
 
 
 if __name__ == "__main__":
