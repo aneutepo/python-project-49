522bc3ddbdd3dc96a1988ae12b5686d7bef8d251
Anton <poverenosn@gmail.com>
2024-08-12 23:03:51 +0300

    fixed text titles

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,6 +3,14 @@ from brain_games.check_user_answer import check_user_answer
 from brain_games.get_username import get_username
 
 
+def create_answer():
+    question_number = random.randint(1, 10000)
+    print(f"Question: {question_number}")
+    user_answer = input("Your answer: ")
+    correct_answer = get_correct_answer(question_number)
+    return correct_answer, user_answer
+
+
 def get_correct_answer(question):
     if question % 2 == 0:
         return 'yes'
@@ -15,10 +23,7 @@ def main():
     print('Answer "yes" if the number is even, otherwise answer "no".')
     score = 0
     while True:
-        question_number = random.randint(1, 10000)
-        print(f"Question: {question_number}")
-        user_answer = input("Your answer: ")
-        correct_answer = get_correct_answer(question_number)
+        correct_answer, user_answer = create_answer()
         if check_user_answer(correct_answer, user_answer, username):
             score += 1
         else:
