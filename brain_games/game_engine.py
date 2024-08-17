98d086c750a271a62b47e7c6aaaea56ae29c1d3f
Anton <poverenosn@gmail.com>
2024-08-17 13:09:48 +0300

    fixed errors

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -6,17 +6,13 @@ def game_process(generate_question_and_answer, game_title):
     username = prompt.string("May I have your name? ")
     print(f"Hello, {username}")
     print(game_title)
-    score = 0
-    while True:
+    for _ in range(3):
         correct_answer, user_answer = generate_question_and_answer()
         if user_answer == str(correct_answer):
             print("Correct!")
-            score += 1
         else:
             print(f"Answer '{user_answer}' is wrong answer ;(. ", end='')
             print(f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {username}!")
             break
-        if score == 3:
-            print(f"Congratulations, {username}!")
-            break
+    print(f'Congratulations, {username}!')
