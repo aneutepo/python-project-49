336f5496c0c21f53ab4b3f598add2f7868a864b7
Anton <poverenosn@gmail.com>
2024-08-20 13:46:05 +0300

    fixed errors

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,15 +1,11 @@
 import random
-GAME_TITLE = "What number is missing in the progression?"
+RULES = "What number is missing in the progression?"
 
 
 def game_data():
     first_element = random.randint(1, 10)
     difference = random.randint(1, 10)
-    progression_numbers = []
-    for i in range(0, 10):
-        progression_numbers.append(first_element)
-        first_element += difference
-
+    progression_numbers = list(range(first_element, 100, difference))[:10]
     number_of_hide_elem = random.randint(0, len(progression_numbers) - 1)
     correct_answer = progression_numbers[number_of_hide_elem]
     progression_numbers[number_of_hide_elem] = '..'
