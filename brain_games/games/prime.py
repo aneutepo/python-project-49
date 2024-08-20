b0edf39d3931acc72ba0adfc953826ad759877cd
Anton <poverenosn@gmail.com>
2024-08-20 13:53:59 +0300

    fixed errors

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,10 +7,7 @@ def is_prime(number):
         return False
     if number == 2:
         return True
-    if number % 2 == 0:
-        return False
-
-    for i in range(3, int(number**0.5) + 1, 2):
+    for i in range(2, int(number**0.5) + 1, 2):
         if number % i == 0:
             return False
     return True
